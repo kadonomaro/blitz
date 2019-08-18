6102e103e908dbefb9c2c1a58c967b7b5973e893
kadonomaro <denis.afer@gmail.com>
2019-08-18 21:38:07 +0700

    added leader section

--- source/js/pixel.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let pixelToggle = document.querySelector('.pixel__check');
-    
-    pixelToggle.addEventListener('change', function () {
-        document.body.classList.toggle('page--pixel');
-    });
-});
