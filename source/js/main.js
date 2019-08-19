d0dbea78c3a19aa062fc92f2c4a35768cddd7ca8
kadonomaro <denis.afer@gmail.com>
2019-08-19 21:49:39 +0700

    added modal js, close button, body scroll lock

--- source/js/main.js
+++ source/js/main.js
@@ -28,9 +28,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     //modal window
+    const callModalButton = document.querySelectorAll('.js-call-request-button');
     const modalOverlay = document.querySelector('.modal-overlay');
     const modal = modalOverlay.querySelector('.modal');
     const modalClose = modal.querySelector('.modal__close');
 
-    
+    callModalButton.forEach(button => {
+        button.addEventListener('click', function () {
+            modalOverlay.style.display = 'block';
+            setTimeout(() => {
+                modalOverlay.classList.add('modal-overlay--active');
+            }, 10);
+            
+            document.body.classList.add('page--modal-is-open');
+        });
+    });
+
+    modalClose.addEventListener('click', function () {
+        modalOverlay.classList.remove('modal-overlay--active');
+        setTimeout(() => {
+            modalOverlay.style.display = 'none';
+        }, 300);
+
+        document.body.classList.remove('page--modal-is-open');
+    });
 });
