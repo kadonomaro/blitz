cfc82405baaf1ad83e030b99e0c728b475cb1660
kadonomaro <denis.afer@gmail.com>
2019-08-19 18:57:01 +0700

    added links hover/active styles, smooth scroll

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,29 @@
-document.addEventListener('DOMContentLoaded')
+document.addEventListener('DOMContentLoaded', function () {
+
+    const navToggle = document.querySelector('.js-nav-toggle');
+    const nav = document.querySelector('.js-nav');
+    const navLinks = nav.querySelectorAll('.js-nav-link');
+
+
+    //nav open/hide
+    navToggle.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        this.classList.toggle('main-nav__toggle--active');
+        nav.classList.toggle('main-nav__wrapper--active');
+    });
+
+
+    //nav links active style and smooth scroll into sections
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            navLinks.forEach(link => {
+                link.classList.remove('main-nav__link--active');
+            });
+            this.classList.add('main-nav__link--active');
+            document.querySelector(`${this.hash}`).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+});
